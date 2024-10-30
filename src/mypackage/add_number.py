6a7d4b9e5026a01a568299fdac1ead1839d2d2a4
MPhilippone <141463422+MPhilippone@users.noreply.github.com>
2024-10-30 16:15:01 +0000

    add: new module that adds numbers together

---
+++ src/mypackage/add_number.py
@@ -0,0 +1,21 @@
+"""A module that adds numbers together"""
+
+def add_num(num_1, num_2):
+""" A function that adds two numbers together
+
+
+Parameters
+__________
+
+num_1 : int
+    The first number that the function will add.
+
+num_2 : int
+    The second number that the function will add.
+
+Return
+______
+    The sum of two numbers
+
+"""
+return num_1 + num_2
